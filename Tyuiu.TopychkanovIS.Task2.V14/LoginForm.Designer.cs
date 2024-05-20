b7d5888b02d758c5f9d6d5f91ccfec4fbd57be0d
Topychkanov Ivan <ogurchikvk0907@gmail.com>
2024-05-20 14:29:19 +0500

    login and registration

--- Tyuiu.TopychkanovIS.Task2.V14/LoginForm.Designer.cs
+++ Tyuiu.TopychkanovIS.Task2.V14/LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tyuiu.TopychkanovIS.Task2.V14
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Tyuiu.TopychkanovIS.Task2.V14/Program.cs
+++ Tyuiu.TopychkanovIS.Task2.V14/Program.cs
@@ -16,7 +16,7 @@ namespace Tyuiu.TopychkanovIS.Task2.V14
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
